74307cccb2a96c30b13ac0b655e6b23c8c3aba37
ShantanuSS <shantanusharma197@gmail.com>
2017-08-15 11:39:45 +0530

    trying to build snake

--- Snake game and AI/snakegame.py
+++ Snake game and AI/snakegame.py
@@ -1,9 +1,40 @@
 from pygame.locals import *
 import pygame
+import time
+
 class Player:
-    x=10
-    y=10
-    speed=1
+    #x=10
+    #y=10
+    x=[]
+    y=[]
+    step=44
+    direction=0
+    #speed=1
+    length=3
+    updatecountmax=2
+    updatecount=0
+    def __init__(self,length):
+        self.length=length
+        for i in range(0,length):
+            self.x.append(0)
+            self.y.append(0)
+    def update(self):
+        self.updatecount+=1
+        if  self.updatecount > self.updatecountmax:
+            for i in range(self.length-1,0,-1):
+                print ("self.x["+ str(i)+"]=self.x["+setr(i-1)+"]")
+                self.x[i]=self.x[i-1]
+                self.y[i]=self.y[i-1]
+
+            if self.direction==0:
+                self.x[0]+=self.step
+            if self.direction==1:
+                self.x[0]-=self.step
+            if self.direction==2:
+                self.y[0]-=self.step
+            if self.direction==3:
+                self.y[0]+=self.step
+            self.updatecount=0
     def moveright(self):
         self.x+=self.speed
     def moveleft(self):
@@ -21,7 +52,7 @@ class SnakeGame:
         self.runningG=True
         self.displayG=None
         self.imageG=None
-        self.player=Player()
+        self.player=Player(10)
     def on_init(self):
         pygame.init()
         self.displayG=pygame.display.set_mode((self.window_width,self.window_height),pygame.HWSURFACE)
@@ -35,7 +66,8 @@ class SnakeGame:
         pass
     def on_render(self):
         self.displayG.fill((0,0,0))
-        self.displayG.blit(self.imageG,(self.player.x,self.player.y))
+        #self.displayG.blit(self.imageG,(self.player.x,self.player.y))
+        self.player.draw(self.displayG,self.imageG)
         pygame.display.flip()
     def on_cleanup(self):
         pygame.quit()
@@ -57,10 +89,10 @@ class SnakeGame:
                 self.runningG=False
             self.on_loop()
             self.on_render()
+        time.sleep(50.0/1000.0)
         self.on_cleanup()
 
 if __name__=="__main__":
     snake=SnakeGame()
     snake.on_execute()
-    
     
