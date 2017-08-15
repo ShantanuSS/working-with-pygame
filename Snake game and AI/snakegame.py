86fbed8465c91c9bbb888f58bd8c19d911ac4698
ShantanuSS <shantanusharma197@gmail.com>
2017-08-15 11:07:30 +0530

    made movable snake

--- Snake game and AI/snakegame.py
+++ Snake game and AI/snakegame.py
@@ -27,22 +27,22 @@ class SnakeGame:
         self.displayG=pygame.display.set_mode((self.window_width,self.window_height),pygame.HWSURFACE)
         pygame.display.set_caption('Snake Game for you') #pyagamedisplay
         self.runningG=True
-        self.imageG=pygame.image.load("abc.png").convert()
+        self.imageG=pygame.image.load("pygame.png").convert()
     def on_event(self,event):
         if event.type==QUIT:
             self.runningG=False
     def on_loop(self):
         pass
     def on_render(self):
-        self.displayG.fill(0,0,0)
-        self.displayG.blit(self.image,(self.player.x,self.player.y))
+        self.displayG.fill((0,0,0))
+        self.displayG.blit(self.imageG,(self.player.x,self.player.y))
         pygame.display.flip()
     def on_cleanup(self):
-        pygamequit()
+        pygame.quit()
     def on_execute(self):
         if self.on_init()==False:
             self.runningG=False
-        while (self.running):
+        while (self.runningG):
             pygame.event.pump()
             keys=pygame.key.get_pressed()
             if(keys[K_RIGHT]):
@@ -57,7 +57,7 @@ class SnakeGame:
                 self.runningG=False
             self.on_loop()
             self.on_render()
-            self.on_cleanup()
+        self.on_cleanup()
 
 if __name__=="__main__":
     snake=SnakeGame()
