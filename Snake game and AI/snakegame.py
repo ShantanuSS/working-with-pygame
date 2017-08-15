148240c445ed1ee8852c1643dc97a0c6ac8357c3
ShantanuSS <shantanusharma197@gmail.com>
2017-08-15 18:53:08 +0530

    implementing food for snake

--- Snake game and AI/snakegame.py
+++ Snake game and AI/snakegame.py
@@ -1,7 +1,15 @@
 from pygame.locals import *
 import pygame
 import time
-
+class Apple:
+    #x=10
+    #y=10
+    step=44
+    def __init__(self,x,y):
+        self.x=x*self.step
+        self.y=y*self.step
+    def draw(self,surface,image):
+        surface.blit(image,(self.x,self,y))
 class Player:
     #x=10
     #y=10
