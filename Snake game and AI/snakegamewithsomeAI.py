5d10ff8a7a3b13098128f35e7416dfdc569c9038
ShantanuSS <shantanusharma197@gmail.com>
2017-08-17 16:14:06 +0530

    code completed ;need to fix bugs now

--- Snake game and AI/snakegamewithsomeAI.py
+++ Snake game and AI/snakegamewithsomeAI.py
@@ -130,33 +130,87 @@ class Game:                                         #handling collision with sna
                 return True
         return False
 
+class SnakeGame:
+    window_width=800
+    window_height=600
+    player=0
+    apple=0
 
-
-
-
-
-
-
-
-
-
+    def __init__(self):
+        self.runningG=True
+        self.displayG=None
+        self.imageG=None
+        self.appleG=None
+        self.game=Game()
+        self.player=Player(5)
+        self.apple=Food(8,5)
+        self.computer=Computer()(5)
+        
+    def on_init(self):
+        pygame.init()
+        self.displayG=pygame.display.set_mode((self.window_width,self.window_height),pygame.HWSURFACE)
+        pygame.display.set_caption('Snake Game for you') 
+        self.runningG=True
+        self.imageG=pygame.image.load("pygame.png").convert()
+        self.appleG=pygame.image.load("foodie.png").convert()
+    def on_event(self,event):
+        if event.type==QUIT:
+            self.runningG=False
     def on_loop(self):
+        self.computer.target(self.apple.x,self.apple.y) #this
         self.player.update()
-        self.computer.update #this
+        self.computer.update()
         #pass
         for i in range(0,self.player.length): 
             if self.game.isCollision(self.apple.x,self.apple.y,self.player.x[i],self.player.y[i],44):
                 self.apple.x=randint(2,9)*44
                 self.apple.y=randint(2,9)*44
                 self.player.length+=1
+        for i in range(0,self.player.length): 
+            if self.game.isCollision(self.apple.x,self.apple.y,self.computer.x[i],self.computer.y[i],44):
+                self.apple.x=randint(2,9)*44
+                self.apple.y=randint(2,9)*44
+                #self.computer.length+=1
         for i in range(2,self.player.length):  
             if self.game.isCollision(self.player.x[0],self.player.y[0],self.player.x[i],self.player.y[i],40):
                 print("You lose! Collision")
                 print("x[0]("+ str(self.player.x[0]) +","+str(self.player.y[0])+")")
                 print("x["+str(i)+"]("+str(self.player.x[i])+","+str(self.player.y[i])+")")
                 exit(0)
-
         pass                
+    def on_render(self):
+        self.displayG.fill((0,0,0))
+        #self.displayG.blit(self.imageG,(self.player.x,self.player.y))
+        self.player.draw(self.displayG,self.imageG)
+        self.apple.draw(self.displayG,self.appleG)
+        self.computer.draw(self.displayG,self.imageG)
+        pygame.display.flip()
+    def on_cleanup(self):
+        pygame.quit()
+    def on_execute(self):
+        if self.on_init()==False:
+            self.runningG=False
+        while (self.runningG):
+            pygame.event.pump()
+            keys=pygame.key.get_pressed()
+            if(keys[K_RIGHT]):
+                self.player.moveright()
+            if(keys[K_LEFT]):
+                self.player.moveleft()
+            if(keys[K_UP]):
+                self.player.moveup()
+            if(keys[K_DOWN]):
+                self.player.movedown()
+            if(keys[K_ESCAPE]):
+                self.runningG=False
+            self.on_loop()
+            self.on_render()
+            time.sleep(50.0/1000.0);
+        self.on_cleanup()
+
+if __name__=="__main__":
+    snake=SnakeGame()
+    snake.on_execute()
     
     
         
